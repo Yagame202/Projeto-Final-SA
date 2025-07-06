71cb870329ee8ffee67423bd6dacb243622b75d8
Yagame <Gustavo_guaresi@estudante.sesisenai.org.br>
2025-07-06 13:08:06 -0300

    Update Monitorar.java

--- Script/Monitorar.java
+++ Script/Monitorar.java
@@ -1,25 +1,41 @@
 package S.A;
 
 public class Monitorar {
+	
+	public static final String BG_RED = "\u001B[41m";
+	public static final String BG_YELLOW = "\u001B[43m";
+    public static final String BG_WHITE = "\u001B[47m";
+	 
+	public static final String BLACK = "\u001B[30m";
+	
+	public static final String RESET = "\u001B[0m";
 
     private boolean estaLigado = false;
 
     public void ligar() {
         if (!estaLigado) {
             estaLigado = true;
-            System.out.println("\n O ar condicionado entrara em modo ventilação.");
+            System.out.println("\n| Ar-condicionado ligado normalmente. |");
+        } else {
+            System.out.println("\n| Ar-condicionado ja esta ligado. |");
         }
     }
     public void desligar() {
         if (estaLigado) {
             estaLigado = false;
-            System.out.println("\n O ar condicionado esta desligado.");
+            System.out.println("\n| Ar-condicionado desligado. |");
         }
     }
-    public void temperatura() {
-    	if(estaLigado) {
-    		estaLigado = true;
-            System.out.println("\n O ar condicionado aumentar a temperatura.");
-    	}
+    public void ajustarTemperatura() {
+        System.out.println(BG_RED + BLACK +"\n| Temperatura muito baixa! Aumentando automaticamente para reduzir o consumo |" + RESET);
+    }
+
+    public void ativarModoVentilacao() {
+        System.out.println(BG_YELLOW + BLACK + "\n| Consumo de energia alto! Ativando modo ventilação |" + RESET);
+    }
+
+    public void atualizarSistema() {
+        System.out.println(BG_WHITE + BLACK + "\n| Sistema atualizado com as novas configurações  |");
     }
 }
+
